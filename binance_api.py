0524cd78336491fa288c6e69a4d60332102eeac6
Walid Madaoui <walidmadaoui@Walids-MacBook-Air.local>
2022-02-20 00:07:25 -0500

    3 timeframes matrices

--- binance_api.py
+++ binance_api.py
@@ -4,10 +4,19 @@ import requests
 fapi_url = "https://fapi.binance.com"
 
 # Response format: open time, open, high, low, close, volume, close time, etc...
-def get_historical_prices(symbol, interval, startTime, endTime):
+def get_historical_prices(symbol, interval, startTime=None, endTime=None):
     route = "/fapi/v1/klines"
+    if startTime and endTime:
+        params={'symbol': symbol, 'interval': interval, 'startTime': startTime, 'endTime': endTime, 'limit': 1000}
+    elif startTime:
+        params={'symbol': symbol, 'interval': interval, 'startTime': startTime, 'limit': 1000}
+    elif endTime:
+        params={'symbol': symbol, 'interval': interval, 'endTime': endTime, 'limit': 1000}
+    else:
+        params={'symbol': symbol, 'interval': interval, 'limit': 1000}
+
     try:
-        resp = requests.get(fapi_url + route, params={'symbol': symbol, 'interval': interval, 'startTime': startTime, 'endTime': endTime, 'limit': 1000})
+        resp = requests.get(fapi_url + route, params=params)
         return resp.json()
     except requests.exceptions.RequestException as e:
         print(e) 
